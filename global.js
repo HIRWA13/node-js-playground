93cba10e679ff2993bde147d85b12c4a90fd79c7
Junior Hirwa <iamhirwejr@gmail.com>
2024-02-22 15:15:47 +0200

    refactor: moved files

--- global.js
+++
@@ -1,9 +0,0 @@
-// Global in node is what the window is to the Broweser
-// Global is the object that holds all the global variables and functions that live in the node js environment.
-
-// __dirname and __filename
-// __dirname - path to the current directory
-// __filename - file name
-
-console.log(__dirname)
-console.log(__filename)
