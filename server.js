4843b12346c5d341a26cc269ee1f9d6b4194b7ae
Junior Hirwa <iamhirwejr@gmail.com>
2024-02-23 16:20:05 +0200

    feat: client - server arch and basic routing

--- server.js
+++ server.js
@@ -4,13 +4,63 @@
 
 // clients vs servers
 
-const http = require('http')
+// status codes
+// most or commonly used are:
+
+/**
+ * 200  -> means that everything is okay with the request
+ *
+ * 301 -> this shows that something was permanently removed and is used to signify a redirect
+ *
+ * 404 -> it means that the file was not found
+ *
+ * 500 -> some kind of an internal server error
+ */
+
+// status code ranges:
+
+/**
+ * 100 range -> these are informational codes
+ *
+ * 200 range -> these are success codes
+ *
+ * 300 range -> codes for redirects
+ *
+ * 400 range -> user or client error codes
+ *
+ * 500 range -> server error codes
+ *
+ */
+
+const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-    console.log('request made successfully')
-})
+  // set headers
+  res.setHeader("Content-Type", "text/html");
 
-server.listen(3000, "localhost", () => {
-  console.log("server running and listening on port 3000");
+  let path = "./views/";
+
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    default:
+      path += "404.html";
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      res.write(err.message);
+      res.end();
+    } else {
+      res.end(data);
+    }
+  });
 });
 
+server.listen(3000);
