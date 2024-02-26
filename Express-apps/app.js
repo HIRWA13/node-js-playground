2a584ee10425d61dc7bc5d179253459812192e7e
Junior Hirwa <iamhirwejr@gmail.com>
2024-02-26 18:55:52 +0200

    feat: add dynamic data

--- Express-apps/app.js
+++ Express-apps/app.js
@@ -7,6 +7,9 @@ const express = require("express");
 // create express app by calling the express function
 const app = express();
 
+// set up the view engine
+app.set('view engine', 'ejs')
+
 // use the express function to create a server listening port number
 app.listen(3000);
 
@@ -17,18 +20,23 @@ app.listen(3000);
 // use the express app to achieve routing
 
 app.get("/", (req, res) => {
-  res.sendFile("./html/index.html", { root: __dirname });
+  const blogs = [
+    { title: 'first blog', snippet: 'a snippet of the first blog' },
+    { title: 'second blog', snippet: 'a snippet of the second blog'},
+    { title: 'third blog', snippet: 'a snippet of the third blog'},
+    { title: 'fourth blog', snippet: 'a snippet of the fourth blog'}
+  ]
+  res.render('index', { title: 'Home', blogs})
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./html/about.html", { root: __dirname });
+  res.render('about', { title: 'About'})
 });
 
-// express redirects: 
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+  res.render('create', {title: 'Create a new Blog'})
 })
 
 app.use((req, res) => {
-    res.status(404).sendfile('./html/404.html', {root: __dirname})
+    res.status(404).render('404', {title: '404'})
 })
