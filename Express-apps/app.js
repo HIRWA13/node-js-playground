3a2803990970179280f17dceb7e7004793e9d27b
Junior Hirwa <iamhirwejr@gmail.com>
2024-03-01 17:43:12 +0200

    feat: add create and get one blog route handlers

--- Express-apps/app.js
+++ Express-apps/app.js
@@ -29,6 +29,8 @@ app.use(morgan("dev"));
 
 app.use(express.static("public"));
 
+app.use(express.urlencoded({extended: true}))
+
 
 // mongoose and mongo sandbox(test) routes
 
@@ -64,6 +66,27 @@ app.get("/", (req, res) => {
 app.get('/blogs', (req, res) => {
   Blog.find().sort({createdAt: -1})
       .then((result) => res.render('index', { title: "All Blogs", blogs: result}))
+      .catch((err) => console.log(err))
+});
+
+
+// create a new blog
+app.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body)
+
+  blog.save()
+      .then((result) => res.redirect('/blogs'))
+      .catch((err) => console.log(err))
+})
+
+// get a single blog based on its id
+
+app.get('/blogs/:id', (req, res) => {
+  const id = req.params.id
+  Blog.findById(id)
+      .then((result) => {
+        res.render('details', {blog: result, title: result.title})
+      })
       .catch((err) => console.log(err))
 })
 
